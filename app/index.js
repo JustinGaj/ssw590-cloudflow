3a3a46e1051396b90d0978d78eaa6c4d51a5fa4f
Justin Gajewski <161546122+JustinGaj@users.noreply.github.com>
2025-12-03 00:21:53 -0500

    basic js frontend for #1

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => res.send('Hello World.'));
+if (require.main === module) app.listen(port, () => console.log(`Listening ${port}`));
+module.exports = app;
